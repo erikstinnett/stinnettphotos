f9e2a9bb8126afea8564dedae1c18afe71de456c
pandawee42 <estinnett@cub.uca.edu>
2022-07-18 22:01:01 -0500

    Merge branch 'main' of https://github.com/pandawee42/websiteproject

--- app.js
+++ app.js
@@ -5,3 +5,27 @@ menu.addEventListener('click', function() {        //Listens for a click
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+let slideIndex = 0;
+showSlides();
+
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
